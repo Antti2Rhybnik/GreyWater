e6f1a06c1e287464ec87e68b64aefa14a32669c4
sapelkinAV <alexander.sapelkin@yahoo.com>
2016-10-12 22:35:35 +0300

    Added LightSensor - looks totally like WaterSensor, but not be fooled it's WaterSensor

--- src/main/java/com/greywater/iot/jpa/LightSensor.java
+++ src/main/java/com/greywater/iot/jpa/LightSensor.java
@@ -1,17 +1,53 @@
 package com.greywater.iot.jpa;
 
+import javax.persistence.*;
+
 /**
  * Created by alexander on 10/12/16.
  */
+@Entity
+@Table(name="LIGHT_SENSOR")
+@NamedQueries({
+        @NamedQuery(name="getAll",query = "SELECT ws from WaterSensor ws")
+})
 public class LightSensor extends VirtualSensor{
+
+    @Column(name="SENSOR_VALUE")
+    private Double sensorValue = null;
+
 
     @Override
     public void addSensorValue(SensorsTableEntity sensorsTableEntity) {
+        this.setgCreated(sensorsTableEntity.getgCreated());
+        this.setSensorValue(sensorsTableEntity.getSensorValue());
+        this.setgDevice(sensorsTableEntity.getgDevice());
+        computeVirtualSensorValue();
 
     }
 
     @Override
     public void computeVirtualSensorValue() {
+        EntityManager entityManager = Persistence.createEntityManagerFactory("GreyWater").createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(this);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 
+    public Double getSensorValue() {
+        return sensorValue;
+    }
+
+    public void setSensorValue(Double sensorValue) {
+        this.sensorValue = sensorValue;
+    }
+
+
+    public double getAverageValue(){
+        EntityManager entityManager = Persistence.createEntityManagerFactory("GreyWater").createEntityManager();
+        Query query = entityManager.createNativeQuery("select avg(ws.sensorValuee) FROM WaterSensor ws");
+        Double average = (Double) query.getSingleResult();
+        entityManager.close();
+        return average;
     }
 }
