e9a1eac5b478cdd06e8c5baa936ebc889424fab2
Andrey Akimov <a.a.m.incorp@yandex.ru>
2016-10-16 19:36:44 +0300

    sensor handler

---
+++ src/main/java/com/greywater/iot/handlers/SensorHandler.java
@@ -0,0 +1,18 @@
+package com.greywater.iot.handlers;
+
+
+public class SensorHandler implements Runnable {
+
+    Long sensor_id;
+
+    public SensorHandler(Long sensor_id) {
+        this.sensor_id = sensor_id;
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+
+}
--- src/main/java/com/greywater/iot/rest/Receiver.java
+++ src/main/java/com/greywater/iot/rest/Receiver.java
@@ -1,16 +1,29 @@
 package com.greywater.iot.rest;
 
 
+import com.greywater.iot.handlers.SensorHandler;
 import com.greywater.iot.utils.SAPStruct;
 
+import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Path("receive")
+@Singleton
 public class Receiver {
+
+
+    ExecutorService es;
+
+    public Receiver() {
+        es = Executors.newSingleThreadExecutor();
+    }
+
 
     // https://greywaterpUSER.hanatrial.ondemand.com/GreyWater/rest/receive/
     @POST
@@ -20,6 +33,8 @@ public class Receiver {
         // здесь мы можем получить сенсор id и запустить что-нибудь
         System.out.println(msg.getSensorID());
         RestApiProvider.tr = msg.getSensorID().toString();
+
+        es.submit(new SensorHandler(msg.getSensorID()));
 
         return Response.ok("200").build();
     }
