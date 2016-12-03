f8d1ad0ed5d8018569a5a14096a5d814c5ef3ebd
MrSandmanRUS <vit34-95@mail.ru>
2016-12-03 21:06:14 +0300

    Update EventNode.java

--- src/main/java/com/greywater/iot/nodeNetwork/EventNode.java
+++ src/main/java/com/greywater/iot/nodeNetwork/EventNode.java
@@ -1,5 +1,9 @@
 package com.greywater.iot.nodeNetwork;
 
+import javax.naming.NamingException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EventNode extends Node<String> {
@@ -8,5 +12,35 @@ public class EventNode extends Node<String> {
     EventNode(List<Node> inputs) {
         super(inputs);
     }
+
+    public static void writeEvent(String id, String type, Connection conn) throws SQLException, NamingException {
+
+        String sqlQuery = "insert into NEO_77I8IO0F4PQ8TZ67A28RD0L2L.EVENTS_TABLE(EVENT_ID, GEN_TIME, IMPORTANCE_RANK, EVENT_TYPE, DIFF) values(?,?,?,?,?)";
+
+        PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
+
+        pstmt.setString(1, id);
+        pstmt.setString(4, type);
+
+        pstmt.executeQuery();
+
+    }
+
+
+    void eval(Connection conn) {
+        inputs.forEach(node -> {
+            if (node.getState() == "1") {
+                try {
+                    writeEvent(node.getId(), node.getType(), conn);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } catch (NamingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
+
+
 
