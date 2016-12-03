e9b1c2325fd63dbfea874b73c56088af910a8c0e
MrSandmanRUS <vit34-95@mail.ru>
2016-12-03 22:21:27 +0300

    Update EventNode.java

--- src/main/java/com/greywater/iot/nodeNetwork/EventNode.java
+++ src/main/java/com/greywater/iot/nodeNetwork/EventNode.java
@@ -1,40 +1,67 @@
 package com.greywater.iot.nodeNetwork;
 
+import com.greywater.iot.persistence.PersistManager;
+
 import javax.naming.NamingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class EventNode extends Node<String> {
 
+    private String importance;
+    private String message;
 
     EventNode(List<Node> inputs) {
         super(inputs);
     }
 
-    public static void writeEvent(String id, String type, Connection conn) throws SQLException, NamingException {
+    EventNode(List<Node> inputs, String importance, String message) {
+        this.importance = importance;
+        this.message = message;
+        this.inputs = inputs;
+    }
 
-        String sqlQuery = "insert into NEO_77I8IO0F4PQ8TZ67A28RD0L2L.EVENTS_TABLE(EVENT_ID, GEN_TIME, IMPORTANCE_RANK, EVENT_TYPE, DIFF) values(?,?,?,?,?)";
+    public void writeEvent(String nodeID, Connection conn) throws SQLException, NamingException {
+
+        String sqlQuery = "insert into NEO_77I8IO0F4PQ8TZ67A28RD0L2L.EVENTS_TABLE(ID, EVENT_TIME, EVENT_MESSAGE, EVENT_IMPORTANCE, NODE_ID) values(?,?,?,?,?)";
 
         PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
 
-        pstmt.setString(1, id);
-        pstmt.setString(4, type);
+        UUID uid = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+        String tableID = uid.randomUUID().toString();
 
+        Calendar calendar = Calendar.getInstance();
+        Date now = calendar.getTime();
+        Timestamp currentTimestamp = new Timestamp(now.getTime());
+
+        pstmt.setString(1, tableID);
+        pstmt.setTimestamp(2, currentTimestamp);
+        pstmt.setString(3, message);
+        pstmt.setString(4, importance);
+        pstmt.setString(5, nodeID);
+        
         pstmt.executeQuery();
-
     }
 
-
-    void eval(Connection conn) {
+    @Override
+    void eval() {
         inputs.forEach(node -> {
-            if (node.getState() == "1") {
+            if ((Boolean)node.getState()) {
+                Connection conn = null;
                 try {
-                    writeEvent(node.getId(), node.getType(), conn);
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                    conn = PersistManager.newConnection();
+                    writeEvent(node.getId(), conn);
+                    conn.close();
                 } catch (NamingException e) {
+                    e.printStackTrace();
+                }
+                catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
