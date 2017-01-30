ca69330f684ec3a02b54a8e6508fef1c1fa42e96
Andrey Akimov <a.a.m.incorp@yandex.ru>
2017-01-30 16:28:16 +0300

    added client v2 files - 2

---
+++ web/client/js/updater.js
@@ -0,0 +1,28 @@
+//обновить и пихнуть данные из файла в графики
+function drawSensorGraph() {
+    updateSensorsData();
+    for (var j = 0; j < sensorsOnGraph.length; j++) { //id датчиков, выведенных на график
+        var data = {
+            series: [
+                []
+            ]
+        };
+        data.series[0] = new Array;
+        for (var i = 0; i < sensorsValue.length; i++) {
+            if (sensorsOnGraph[j] == sensorsValue[i].sensorId) {
+                var tmp = { x: sensorsValue[i].gCreated, y: sensorsValue[i].sensorValue };
+                data.series[0].push(tmp);
+            }
+        }
+        var options = {
+            height: 200,
+            axisX: {
+                type: Chartist.AutoScaleAxis,
+                onlyInteger: true
+            }
+        };
+        new Chartist.Line('.graph' + j, data, options);
+    }
+}
+
+setInterval(drawSensorGraph, timeOfGraphReload);
